3d52b01331d533578754a4df64170dd15aa2114f
Felipe <felipe.automacao2017@gmail.com>
2020-11-25 12:17:19 -0300

    fix: corrigir exercício 40 feat: adicionar exercício 61

--- src/com/example/exercicios/selecao/Exercicio40.kt
+++ src/com/example/exercicios/selecao/Exercicio40.kt
@@ -10,15 +10,15 @@ fun main(){
     var media: Float= ((n1*0.2)+(n2*0.3)+(n3*0.4)+(n4*0.1)).toFloat()
 
     when (media){
-        in 0.0..5.0 -> print("Media: $media \nAluno reprovado")
+        in 0.0..5.0 -> print("Media: %.2f".format(media) + "\nAluno reprovado")
         in 5.0..7.0 ->{
-            print("Media: $media \nAluno em exame\n")
+            print("Media: %.2f".format(media) + "\nAluno em exame\n")
             nExame = readLine().toString().toFloat()
             media= (media+nExame)/2
-            if(media>=5.0) println("Nota do exame: $nExame \nAluno aprovado \nMedia final: $media")
-            else println("Nota do exame: $nExame \nAluno reprovado \nMedia final: $media")
+            if(media>=5.0) println("Nota do exame: %.2f".format(nExame) + "\nAluno aprovado \nMedia final: %.2f".format(media))
+            else println("Nota do exame: %.2f".format(nExame) + "\nAluno reprovado \nMedia final: %.2f".format(media))
         }
-        in 7.0..10.0 -> print("Media: $media \nAluno aprovado")
+        in 7.0..10.0 -> print("Media: %.2f".format(media) + " \nAluno aprovado")
 
     }
 
